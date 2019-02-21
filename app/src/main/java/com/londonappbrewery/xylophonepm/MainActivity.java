f17f8bb86ee803ed0f61420f53c461d1d41e9cba
londonappbrewery <londonappbrewery@gmail.com>
2019-02-21 11:02:56 +0000

    updating stub

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -8,22 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Helpful Constants
-    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
-    private final float LEFT_VOLUME = 1.0f;
-    private final float RIGHT_VOLUME = 1.0f;
-    private final int NO_LOOP = 0;
-    private final int PRIORITY = 0;
-    private final float NORMAL_PLAY_RATE = 1.0f;
-
     // TODO: Add member variables here
-    private int mCSoundId;
-    private int mDSoundId;
-    private int mESoundId;
-    private int mFSoundId;
-    private int mGSoundId;
-    private int mASoundId;
-    private int mBSoundId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
