254a2e10f73530a262905ddd9262e21ff262028a
= <=>
2019-03-01 13:44:48 +0000

    Changing package name

--- app/src/main/java/net/jws7/xylophonepm/MainActivity.java
+++ app/src/main/java/net/jws7/xylophonepm/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.londonappbrewery.xylophonepm.R;
+import net.jws7.xylophonepm.R;
 
 public class MainActivity extends AppCompatActivity {
 
